e9969976ffe1ada676057958999959bbef693384
LexaZetLot <95221911+LexaZetLot@users.noreply.github.com>
2023-08-04 01:18:23 +0300

    Thomas-Corman
    Thomas-Corman

---
+++ Алгоритмы построение и анализ/Глава 2/Алгоритмы/insertion_sort.c
@@ -0,0 +1,32 @@
+#define SIZE 7
+
+void insertion_sort (int * arr, int N);
+
+int main (void)
+{
+    int arr [SIZE] = {4, 7, 2, 1, 56, 93, 0};
+    
+    insertion_sort (arr, SIZE);
+
+    return 0;
+}
+
+
+void insertion_sort (int * arr, int N)
+{
+    int i;
+    int j;
+    int key;
+    
+    for (i = 1; i < N; i++)
+    {
+        key = arr [i];
+        j = i - 1;
+        while (arr [j] > key)
+        {
+            arr [j + 1] = arr [j];
+            --j;
+        }
+        arr [j + 1] = key;
+    }
+}

31ad86c4052b9edbbaa54996b5d26756a7f75bdd
LexaZetLot <95221911+LexaZetLot@users.noreply.github.com>
2023-08-20 21:44:51 +0300

    Thomas-Cormen
    Thomas-Cormen

--- Алгоритмы построение и анализ/Глава 4/Алгоритмы/strassen's_algorithm.c
+++ Алгоритмы построение и анализ/Глава 4/Алгоритмы/strassen's_algorithm.c
@@ -6,50 +6,40 @@
 #define SIZE_COLUMN_A 2
 #define SIZE_COLUMN_B 2
 
-int ** square_matrix_multiply_recursive (int ** arr_a, int ** arr_b, int ** arr_c, int N);
-int recursive (int a, int b);
+int square_matrix_multiply_recursive (int * arr_a, int * arr_b, int * arr_c, int N);
 
 int main (void)
 {
-    int ** arr_a;
-    arr_a = (int**) calloc (SIZE_STRING_A, sizeof (int*));
-    for (int i = 0; i < SIZE_STRING_A; i++)
-        arr_a [i] = (int*) calloc (SIZE_COLUMN_A, sizeof (int));
+    int * arr_a = (int*) calloc (SIZE_STRING_A * SIZE_COLUMN_A, sizeof (int));
+    
     for (int i = 0; i < SIZE_STRING_A; i++)
         for (int j = 0; j < SIZE_COLUMN_A; j++)
-            arr_a [i] [j] = i + j;
+            arr_a [i * 2 + j] = i + j;
     for (int i = 0; i < SIZE_STRING_A; i++)
     {
         for (int j = 0; j < SIZE_COLUMN_B; j++)
-            printf ("%d ", arr_a [i] [j]);
+            printf ("%d ", arr_a [i* 2 + j]);
         printf ("\n");
     }
     
-    int ** arr_b;
-    arr_b = (int**) calloc (SIZE_STRING_B, sizeof (int*));
-    for (int i = 0; i < SIZE_STRING_B; i++)
-        arr_b [i] = (int*) calloc (SIZE_COLUMN_B, sizeof (int));
+    int * arr_b = (int*) calloc (SIZE_STRING_B * SIZE_COLUMN_B, sizeof (int));
     for (int i = 0; i < SIZE_STRING_B; i++)
         for (int j = 0; j < SIZE_COLUMN_B; j++)
-            arr_b [i] [j] = i + j;
+            arr_b [i * 2 + j] = i + j;
     for (int i = 0; i < SIZE_STRING_A; i++)
     {
         for (int j = 0; j < SIZE_COLUMN_B; j++)
-            printf ("%d ", arr_b [i] [j]);
+            printf ("%d ", arr_b [i * 2 + j]);
         printf ("\n");
     }
-    
-    int ** arr_c;
-    arr_c = (int**) calloc (SIZE_STRING_A, sizeof (int*));
-    for (int i = 0; i < SIZE_STRING_B; i++)
-        arr_c [i] = (int*) calloc (SIZE_COLUMN_B, sizeof (int));
 
+    int * arr_c = (int*) calloc (SIZE_STRING_A * SIZE_COLUMN_B, sizeof (int*));
     square_matrix_multiply_recursive (arr_a, arr_b, arr_c, 2);
     
     for (int i = 0; i < SIZE_STRING_A; i++)
     {
         for (int j = 0; j < SIZE_COLUMN_B; j++)
-            printf ("%d ", arr_c [i] [j]);
+            printf ("%d ", arr_c [i * 2 + j]);
         printf ("\n");
     }
 
@@ -57,27 +47,20 @@ int main (void)
 }
 
 
-int ** square_matrix_multiply_recursive (int ** arr_a, int ** arr_b, int ** arr_c, int N)
+int square_matrix_multiply_recursive (int * arr_a, int * arr_b, int * arr_c, int N)
 {
     if (N == 1)
     {
-        printf ("***%d***\n", **arr_b);
-        (**arr_c) += (**arr_a); //* (**arr_b);
-        return arr_c;
+        int bufer;
+        bufer = (*arr_b) * (*arr_a);
+        return bufer;
     }
     else 
     {
-        arr_c [0] [0] = (**square_matrix_multiply_recursive (&(*(arr_a + 0)) + 0, &(*(arr_b + 0)) + 0, &(*(arr_c + 0)) + 0, N / 2)) + (**square_matrix_multiply_recursive (&(*(arr_a + 0)) + 1, &(*(arr_b + 1)) + 0, &(*(arr_c + 0)) + 0, N / 2));
-        printf ("%d  %p\n",  arr_c [0] [0], &(*(arr_c + 0)) + 0);
-        printf ("%d  %p\n",  arr_c [1] [0], &(*(arr_c + 1)) + 0);
-        printf ("%d  %p\n",  arr_c [1] [1], &(*(arr_c + 1)) + 1);
-        arr_c [0] [1] = (**square_matrix_multiply_recursive (&(*(arr_a + 0)) + 0, &(*(arr_b + 0)) + 1, &(*(arr_c + 0)) + 1, N / 2)) + (**square_matrix_multiply_recursive (&(*(arr_a + 0)) + 1, (*(arr_b + 1) + 1), &(*(arr_c + 0)) + 1, N / 2));
-        //arr_c [1] [0] = square_matrix_multiply_recursive (arr_a [1] [0], arr_b [0] [0], arr_c [1] [0], N / 2) + square_matrix_multiply_recursive (arr_a [1] [1], arr_b [1] [0], arr_c [1] [0], N / 2);
-        //arr_c [1] [1] = square_matrix_multiply_recursive (arr_a [1] [0], arr_b [0] [0], arr_c [1] [1], N / 2) + square_matrix_multiply_recursive (arr_a [1] [1], arr_b [1] [1], arr_c [1] [1], N / 2);
+        *(arr_c + (0 * N + 0)) = square_matrix_multiply_recursive (arr_a + (0 * N + 0), arr_b + (0 * N + 0), arr_c + (0 * N + 0), N / 2) + square_matrix_multiply_recursive (arr_a + (0 * N + 1), arr_b + (1 * N + 0), arr_c + (0 * N + 0), N / 2);
+        *(arr_c + (0 * N + 1)) = square_matrix_multiply_recursive (arr_a + (0 * N + 0), arr_b + (0 * N + 1), arr_c + (0 * N + 1), N / 2) + square_matrix_multiply_recursive (arr_a + (0 * N + 1), arr_b + (1 * N + 1), arr_c + (0 * N + 1), N / 2);
+        *(arr_c + (1 * N + 0)) = square_matrix_multiply_recursive (arr_a + (1 * N + 0), arr_b + (0 * N + 0), arr_c + (1 * N + 0), N / 2) + square_matrix_multiply_recursive (arr_a + (1 * N + 1), arr_b + (1 * N + 0), arr_c + (1 * N + 0), N / 2);
+        *(arr_c + (1 * N + 1)) = square_matrix_multiply_recursive (arr_a + (1 * N + 0), arr_b + (0 * N + 1), arr_c + (0 * N + 0), N / 2) + square_matrix_multiply_recursive (arr_a + (1 * N + 1), arr_b + (1 * N + 1), arr_c + (1 * N + 1), N / 2);
+        
     }
-}
-
-int recursive (int a, int b)
-{
-    return a * b;
-}
+    return 1;

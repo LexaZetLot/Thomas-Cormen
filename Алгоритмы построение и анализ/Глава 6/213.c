f4b1554e7e9233616340d2c37615e74cfed328a0
My name is Alexey, I'm 20 years old and I'm a C (in a greater degree)/C++ developer <95221911+LexaZetLot@users.noreply.github.com>
2023-11-04 23:03:47 +0300

    Thomas-Cormen
    Thomas-Cormen

--- Алгоритмы построение и анализ/Глава 6/213.c
+++ Алгоритмы построение и анализ/Глава 6/213.c
@@ -1,12 +1,14 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
+##include <stdio.h>
 #include <stdlib.h>
 #define N 3
-#define K 2
 
-int flag_size = 1;
+struct hope_list
+{
+	int key;
+	int data;
+};
 
-int heap_maximum(int* arr);
+int heap_maximum(struct hope_list* list, int size);
 int heap_extract_max(int* arr, int size);
 void heap_sort_min_max(int* arr, int size);
 void heap_sort_max_min(int* arr, int size);
@@ -15,78 +17,71 @@ void min_haep_recursion(int* arr, int root, int size);
 
 int main(void)
 {
-	int* arr;
-	arr = (int*)malloc((N * K) * sizeof(int));
+	struct hope_list list[N];
 
 	printf("KEY   DATA\n");
 	for (int i = 0; i < N; i++)
-	{
-		scanf("%d", &arr[N * 0 + i]);
-		scanf("%d", &arr[N * 1 + i]);
-	}
-	for (int i = 0; i <= 3; i++)
-		printf("%d\n", heap_extract_max(arr, N - i));
-	//printf("%d\n", heap_extract_max(arr, N));
-	//printf("%d\n", heap_extract_max(arr, N - 2));
-	//printf("%d\n", heap_extract_max(arr, N - 3));
-	//printf("\n");
-	//for (int i = 0; i < N; i++)
-	//	printf("%d   %d\n", arr[N * 0 + i], arr[N * 1 + i]);
+		scanf("%d%d", &list[i].key, &list[i].data);
+
+	printf("%d", heap_maximum(list));
+
+	//for (int i = 0; i <= 3; i++)
+	//	printf("%d\n", heap_extract_max(arr, (N / 2) - i));
+
+	//for (int i = 0; i < N / 2; i++)
+	//	printf("%d   %d\n", arr[i], arr[i + 1]);
 	return 0;
 }
 
 
 
-int heap_maximum(int* arr)
+int heap_maximum(struct hope_list* list, int size)
 {
-	return arr[1];
+	heap_sort_max_min(list, size);
+	return arr[0];
 }
 
 void max_haep_recursion(int* arr, int root, int size)
 {
-	if (size < 2)
-		return;
-	else
-	{
-		int max_number = root;
-		int left = 2 * root;
-		int right = 2 * root + 1;
-		int* bufer;
-		bufer = (int*)malloc(K * sizeof(int));
 
-		if (left < size && arr[N * 0 + left] > arr[N * 0 + max_number])
-			max_number = left;
-		if (right < size && arr[N * 0 + right] > arr[N * 0 + max_number])
-			max_number = right;
-		if (max_number != root)
-		{
-			bufer[0] = arr[N * 0 + root];
-			bufer[1] = arr[N * 1 + root];
-			arr[N * 0 + root] = arr[N * 0 + max_number];
-			arr[N * 1 + root] = arr[N * 1 + max_number];
-			arr[N * 0 + max_number] = bufer[0];
-			arr[N * 1 + max_number] = bufer[1];
-			max_haep_recursion(arr, max_number, size);
-		}
-		free(bufer);
+	int max_number = root;
+	int left = 2 * root;
+	int right = 2 * root + 1;
+	int* bufer;
+	bufer = (int*)malloc(2 * sizeof(int));
+
+	if (left < size && arr[left] > arr[max_number])
+		max_number = left;
+	if (right < size && arr[right] > arr[max_number])
+		max_number = right;
+	if (max_number != root)
+	{
+		bufer[0] = arr[root];
+		bufer[1] = arr[root + 1];
+		arr[root] = arr[max_number];
+		arr[root + 1] = arr[max_number + 1];
+		arr[max_number] = bufer[0];
+		arr[max_number + 1] = bufer[1];
+		max_haep_recursion(arr, max_number, size);
 	}
+	free(bufer);
 }
 
 void heap_sort_min_max(int* arr, int size)
 {
 	int* bufer;
-	bufer = (int*)malloc(K * sizeof(int));
+	bufer = (int*)malloc(2 * sizeof(int));
 
 	for (int i = size / 2 - 1; i >= 0; i--)
 		min_haep_recursion(arr, i, size);
 	for (int i = size - 1; i >= 0; i--)
 	{
-		bufer[0] = arr[N * 0];
-		bufer[1] = arr[N * 1];
-		arr[N * 0 + 0] = arr[N * 0 + i];
-		arr[N * 1 + 0] = arr[N * 1 + i];
-		arr[N * 0 + i] = bufer[0];
-		arr[N * 1 + i] = bufer[1];
+		bufer[0] = arr[0];
+		bufer[1] = arr[1];
+		arr[0] = arr[i];
+		arr[1] = arr[i + 1];
+		arr[i] = bufer[0];
+		arr[i + 1] = bufer[1];
 		min_haep_recursion(arr, 0, i);
 	}
 	free(bufer);
@@ -94,22 +89,20 @@ void heap_sort_min_max(int* arr, int size)
 
 void heap_sort_max_min(int* arr, int size)
 {
-	int* bufer;
-	bufer = (int*)malloc(K * sizeof(int));
+	struct hope_list bufer;
 
 	for (int i = size / 2 - 1; i >= 0; i--)
 		max_haep_recursion(arr, i, size);
 	for (int i = size - 1; i >= 0; i--)
 	{
-		bufer[0] = arr[N * 0];
-		bufer[1] = arr[N * 1];
-		arr[N * 0 + 0] = arr[N * 0 + i];
-		arr[N * 1 + 0] = arr[N * 1 + i];
-		arr[N * 0 + i] = bufer[0];
-		arr[N * 1 + i] = bufer[1];
+		bufer[0] = arr[0];
+		bufer[1] = arr[1];
+		arr[0] = arr[i];
+		arr[1] = arr[i + 1];
+		arr[i] = bufer[0];
+		arr[i + 1] = bufer[1];
 		max_haep_recursion(arr, 0, i);
 	}
-	free(bufer);
 }
 
 void min_haep_recursion(int* arr, int root, int size)
@@ -118,20 +111,20 @@ void min_haep_recursion(int* arr, int root, int size)
 	int left = 2 * root;
 	int right = 2 * root + 1;
 	int* bufer;
-	bufer = (int*)malloc(K * sizeof(int));
+	bufer = (int*)malloc(2 * sizeof(int));
 
-	if (left < size && arr[N * 0 + left] < arr[N * 0 + min_number])
+	if (left < size && arr[left] < arr[min_number])
 		min_number = left;
-	if (right < size && arr[N * 0 + right] < arr[N * 0 + min_number])
+	if (right < size && arr[right] < arr[min_number])
 		min_number = right;
 	if (min_number != root)
 	{
-		bufer[0] = arr[N * 0 + root];
-		bufer[1] = arr[N * 1 + root];
-		arr[N * 0 + root] = arr[N * 0 + min_number];
-		arr[N * 1 + root] = arr[N * 1 + min_number];
-		arr[N * 0 + min_number] = bufer[0];
-		arr[N * 1 + min_number] = bufer[1];
+		bufer[0] = arr[root];
+		bufer[1] = arr[root + 1];
+		arr[root] = arr[min_number];
+		arr[root + 1] = arr[min_number + 1];
+		arr[min_number] = bufer[0];
+		arr[min_number + 1] = bufer[1];
 		min_haep_recursion(arr, min_number, size);
 	}
 }
@@ -139,20 +132,16 @@ void min_haep_recursion(int* arr, int root, int size)
 int heap_extract_max(int* arr, int size)
 {
 	if (size < 1)
-	{
-		printf("Очередь пуста");
-		return 0;
-	}
+		return printf("Очередь пуста\n");
+
+	getchar();
+	printf("%d ", arr[0]);
 	heap_sort_min_max(arr, size);
 	int max;
-
-	max = arr[0 * N + 0];
-	arr[0 * N + 0] = arr[0 * N + size];
-	arr[1 * N + 0] = arr[1 * N + size];
+	max = arr[0];
+	arr[0] = arr[size * 2 - 1];
+	arr[1] = arr[size * 2];
 	arr = (int*)realloc(arr, (size - 1) * 2 * sizeof(int));
-	printf("%d ", arr[size - 1]);
 	max_haep_recursion(arr, 0, size - 1);
-	flag_size++;
 	return max;
 }
-

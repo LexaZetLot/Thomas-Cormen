b03d8b7f4ba68b4d57f075f602da69a9ccbbc2f9
My name is Alexey, I'm 20 years old and I'm a C (in a greater degree)/C++ developer <95221911+LexaZetLot@users.noreply.github.com>
2023-11-13 22:23:19 +0300

    Thomas-Cormen
    Thomas-Cormen

--- Алгоритмы построение и анализ/Глава 6/Jungian_table.c
+++ Алгоритмы построение и анализ/Глава 6/Jungian_table.c
@@ -4,21 +4,23 @@
 #define N 3
 #define M 4
 
+int* sort(int* arr, int size);
+int search(int* arr, int num, int x, int y);
 int heap_extract_min(int* arr, int size);
+void heap_sort_max_min(int* arr, int size);
 void heap_sort_min_max(int* arr, int size);
 void min_haep_recursion(int* arr, int root, int size);
 void max_haep_recursion(int* arr, int root, int size);
+void max_heap_insert(int* arr, int key, int size);
 
 int main(void)
 {
-	int a = N * M;
+	int a = 1;
 	int* arr;
 	arr = (int*)malloc(N * M * sizeof(int));
 
-	for (int i = 0; i < N * M; i++, a--)
+	for (int i = 0; i < N * M; i++, a++)
 		arr[i] = a;
-
-	a = heap_extract_min(arr, N * M);
 	for (int i = 0; i < N * M; i++)
 	{
 		if ((i % 4 == 0) && (i != 0))
@@ -26,7 +28,14 @@ int main(void)
 		printf("%d ", arr[i]);
 
 	}
-	printf("\n%d", a);
+	printf("\n%d\n", search(arr, 7, N, M));
+	//for (int i = 0; i < N * M; i++)
+	//{
+	//	if ((i % 4 == 0) && (i != 0))
+	//		printf("\n");
+	//	printf("%d ", arr[i]);
+	//
+	//}
 	return 0;
 }
 
@@ -36,10 +45,10 @@ int heap_extract_min(int* arr, int size)
 	if (size <= 1)
 		exit(0);
 
-	heap_sort_min_max(arr, size);
+	heap_sort_max_min(arr, size);
 	bufer = arr[size - 1];
 	arr[size - 1] = INT_MAX;
-	max_haep_recursion(arr, 0, size - 1);
+	heap_sort_min_max(arr, size);
 	return bufer;
 
 
@@ -60,6 +69,21 @@ void heap_sort_min_max(int* arr, int size)
 	}
 }
 
+void heap_sort_max_min(int* arr, int size)
+{
+	int bufer;
+
+	for (int i = size / 2 - 1; i >= 0; i--)
+		max_haep_recursion(arr, i, size);
+	for (int i = size - 1; i >= 0; i--)
+	{
+		bufer = arr[0];
+		arr[0] = arr[i];
+		arr[i] = bufer;
+		max_haep_recursion(arr, 0, i);
+	}
+}
+
 void min_haep_recursion(int* arr, int root, int size)
 {
 	int min_number = root;
@@ -67,9 +91,9 @@ void min_haep_recursion(int* arr, int root, int size)
 	int right = 2 * root + 2;
 	int bufer;
 
-	if (left < size && arr[left] < arr[min_number] && arr[left] != INT_MAX)
+	if (left < size && arr[left] > arr[min_number])
 		min_number = left;
-	if (right < size && arr[right] < arr[min_number] && arr[right] != INT_MAX)
+	if (right < size && arr[right] > arr[min_number])
 		min_number = right;
 	if (min_number != root)
 	{
@@ -87,9 +111,9 @@ void max_haep_recursion(int* arr, int root, int size)
 	int right = 2 * root + 2;
 	int bufer;
 
-	if (left < size && arr[left] > arr[max_number] && arr[left] != INT_MAX)
+	if (left < size && arr[left] < arr[max_number])
 		max_number = left;
-	if (right < size && arr[right] > arr[max_number] && arr[right] != INT_MAX)
+	if (right < size && arr[right] < arr[max_number])
 		max_number = right;
 	if (max_number != root)
 	{
@@ -97,5 +121,45 @@ void max_haep_recursion(int* arr, int root, int size)
 		arr[root] = arr[max_number];
 		arr[max_number] = bufer;
 		max_haep_recursion(arr, max_number, size);
+	}
+}
+
+void max_heap_insert(int* arr, int key, int size)
+{
+	if (arr[size - 1] != INT_MAX)
+		exit(2);
+
+	arr[size - 1] = key;
+	heap_sort_min_max(arr, size);
+}
+
+int* sort(int* arr, int size)
+{
+	int* arr_return;
+	arr_return = (int*)malloc(size * sizeof(int));
+
+	for (int i = 0; i < size; i++)
+		arr_return[i] = heap_extract_min(arr, size);
+
+	return arr_return;
+}
+
+int search(int* arr, int num, int x, int y)
+{
+	int i = 0;
+	int j = 0;
+	while (true)
+	{
+		if (arr[y * i + x + j] > num)
+		{
+			i++;
+			j = 0;
+		}
+		if (arr[y * i + x + j] < num)
+			j++;
+		if (arr[y * i + x + j] == num)
+			return y * i + x + j;
+		if (i == y - 1 && arr[y * x] != num)
+			return -1;
 	}
 }

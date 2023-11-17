d02dd6720fa959429868ac1d35f3cc9f602cd194
My name is Alexey, I'm 20 years old and I'm a C (in a greater degree)/C++ developer <95221911+LexaZetLot@users.noreply.github.com>
2023-11-17 23:52:48 +0300

    Thomas-Cormen
    Thomas-Cormen

---
+++ Алгоритмы построение и анализ/Глава 7/rand_quicksortI.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define N 100
+
+void swap(int* x, int* y);
+void rand_quicksotrI(int* arr, int size);
+void rand_quicksotr_recurionI(int* arr, int low, int high);
+void rand_partitionI(int* arr, int low, int high, int& begin, int& end);
+
+
+int main(void)
+{
+
+	int* arr;
+	arr = (int*)malloc(N * sizeof(int));
+	srand((unsigned int)time(NULL));
+
+	for (int i = 0; i < N; i++)
+		arr[i] = rand();
+	rand_quicksotrI(arr, N);
+	for (int i = 0; i < N; i++)
+		printf("%d ", arr[i]);
+	return 0;
+}
+
+void swap(int* x, int* y)
+{
+	int bufer = *x;
+	*x = *y;
+	*y = bufer;
+}
+
+void rand_quicksotrI(int* arr, int size)
+{
+	srand((unsigned int)time(NULL));
+	rand_quicksotr_recurionI(arr, 0, size - 1);
+}
+
+void rand_quicksotr_recurionI(int* arr, int low, int high)
+{
+	if (low < high)
+	{
+		int begin;
+		int end;
+		rand_partitionI(arr, low, high, begin, end);
+		rand_quicksotr_recurionI(arr, low, --begin);
+		rand_quicksotr_recurionI(arr, ++end, high);
+	}
+}
+
+void rand_partitionI(int* arr, int low, int high, int& begin, int& end)
+{
+	int pivot_index;
+	pivot_index = low + (rand() % (high - low));
+	if (pivot_index != high)
+		swap(arr + pivot_index, arr + low);
+
+	int pivot = arr[low];
+	begin = low;
+	end = high;
+
+	for (int i = begin + 1; i <= end; i++)
+	{
+		if (arr[i] < pivot)
+		{
+			swap(arr + i, arr + begin);
+			begin++;
+		}
+		if (arr[i] > pivot)
+		{
+			swap(arr + i, arr + end);
+			end--;
+			i--;
+		}
+	}
+}

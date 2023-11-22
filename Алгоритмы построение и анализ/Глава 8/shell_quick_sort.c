45d53e62b3fccac0762053938fa6958d356fb7d1
My name is Alexey, I'm 20 years old and I'm a C (in a greater degree)/C++ developer <95221911+LexaZetLot@users.noreply.github.com>
2023-11-22 21:23:15 +0300

    Thomas-Cormen
    Thomas-Cormen

---
+++ Алгоритмы построение и анализ/Глава 8/shell_quick_sort.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define N 100
+
+void shell_sort(int* arr, int size);
+void swap(int* x, int* y);
+void rand_quicksotr(int* arr, int size);
+void rand_quicksotr_recurion(int* arr, int low, int high);
+int rand_partition(int* arr, int low, int high);
+
+int main(void)
+{
+	int* arr;
+	arr = (int*)malloc(N * sizeof(int));
+	srand((unsigned int)time(NULL));
+	for (int i = 0; i < N; i++)
+		arr[i] = rand();
+	for (int i = 0; i < N; i++)
+		printf("%d ", arr[i]);
+	printf("\n********************\n");
+	shell_sort(arr, N);
+	for (int i = 0; i < N; i++)
+		printf("%d ", arr[i]);
+
+	free(arr);
+	return 0;
+}
+
+void swap(int* x, int* y)
+{
+	int bufer = *x;
+	*x = *y;
+	*y = bufer;
+}
+
+void shell_sort(int* arr, int size)
+{
+	for (int i = size / 2; i > 0; i /= 2)
+		rand_quicksotr(arr + i - 1, size - i);
+}
+
+void rand_quicksotr(int* arr, int size)
+{
+	srand((unsigned int)time(NULL));
+	rand_quicksotr_recurion(arr, 0, size - 1);
+}
+
+void rand_quicksotr_recurion(int* arr, int low, int high)
+{
+	if (low < high)
+	{
+		int pivot = rand_partition(arr, low, high);
+		rand_quicksotr_recurion(arr, low, pivot - 1);
+		rand_quicksotr_recurion(arr, pivot + 1, high);
+	}
+}
+
+int rand_partition(int* arr, int low, int high)
+{
+	int pivot_index;
+	pivot_index = low + (rand() % (high - low));
+	if (pivot_index != high)
+		swap(arr + pivot_index, arr + high);
+
+	int pivot = arr[high];
+	int j = low;
+	for (int i = low; i < high; i++)
+		if (arr[i] <= pivot)
+		{
+			swap(arr + j, arr + i);
+			j++;
+		}
+
+	swap(arr + j, arr + high);
+
+	return j;
+}
